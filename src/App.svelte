9e541fd35463ad8bdf3fa7ca1d037ba9aca4daf3
cartereric <eric.eugene.carter@gmail.com>
2021-06-22 13:48:55 -0400

    Update img src for production

--- src/App.svelte
+++ src/App.svelte
@@ -14,7 +14,7 @@
     }
     @font-face {
         font-family: "Bananas-Personal";
-        src: url("https://polyglot-spa-navbar.s3.us-east-2.amazonaws.com/dist/assets/fonts/BananasPersonalUse.ttf") format("truetype");
+        src: url("https://polyglot-spa-navbar.s3.us-east-2.amazonaws.com/assets/fonts/BananasPersonalUse.ttf") format("truetype");
         /*src: url("http://localhost:5002/assets/fonts/BananasPersonalUse.ttf") format("truetype");*/
     }
     #headerText {
